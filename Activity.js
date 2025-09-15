c5d68c16c88b757f46817d401f22be945e13aa4b
kyee-ux <kyleinday2015@gmail.com>
2025-09-15 10:11:55 +0800

    first commit

---
+++ Activity.js
@@ -0,0 +1,14 @@
+let age = parseInt(promt("Enter your age: "));
+let hasVIP = prompt("Do you have a VIP pass? ");
+
+if(age < 3){
+    console.log("Free Entry");
+}else if(age <= 12){
+    return $10;
+}else if(age <= 59){
+    return $20;
+}else if(age > 60){
+    return $5;
+}else if(age = VIP){
+    return "You get a free snacks";
+}
